9d7787437775a9c58c794527d200c2bcd55c65d7
siyao <sh2435@cornell.edu>
2019-08-26 14:02:19 +0000

    react app

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { connect, sendMsg } from "./api";
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    connect()
+  }
+  
+  send(){
+    console.log("hello")
+    sendMsg("hello")
+  }
   render() {
     return (
+
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <body>
+          The content of the document......
+        </body>
+        <button onClick={this.send}>Hit</button>
       </div>
     );
   }
